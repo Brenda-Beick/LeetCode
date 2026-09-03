7cde49cbe9e978c7a4e0350743765d219b514b31
Brenda Beick <113392905+Brenda-Beick@users.noreply.github.com>
2026-09-03 12:46:44 -0300

    Time: 2 ms (42.2%), Space: 11 MB (89.47%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.c
@@ -0,0 +1,26 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize) {
+    *returnSize = n;
+    char **answer = (char**)malloc(n*sizeof(char*));
+
+    for(int i = 1; i <= n; i++){
+        answer[i-1] = malloc(9*sizeof(char));
+        if((i%3 == 0) && (i%5 == 0)){
+             answer[i-1] = "FizzBuzz";
+             } else{
+                if(i%3 == 0){ 
+                    answer[i-1] = "Fizz";
+                }else{
+                    if(i%5 == 0){
+                       answer[i-1] = "Buzz";
+                        } else{
+                            sprintf(answer[i-1],"%d",i);
+                            }
+                        }
+                    }
+                           
+    }
+    return answer;
+}
